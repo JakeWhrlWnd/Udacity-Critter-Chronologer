18b13b796b78b07a5adf03fa2b709a588679e3cf
JakeWhrlWnd <justjamescreative@outlook.com>
2023-01-08 10:15:48 -0500

    Updated

--- starter/critter/src/main/java/com/udacity/jdnd/course3/critter/repository/CustomerRepository.java
+++ starter/critter/src/main/java/com/udacity/jdnd/course3/critter/repository/CustomerRepository.java
@@ -4,9 +4,10 @@ package com.udacity.jdnd.course3.critter.repository;
 import com.udacity.jdnd.course3.critter.entity.Customer;
 import org.springframework.data.jdbc.repository.query.Query;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
-
+@Repository
 public interface CustomerRepository extends JpaRepository<Customer, Long> {
 
     @Override
--- starter/critter/src/main/java/com/udacity/jdnd/course3/critter/repository/ScheduleRepository.java
+++ starter/critter/src/main/java/com/udacity/jdnd/course3/critter/repository/ScheduleRepository.java
@@ -4,9 +4,10 @@ import com.udacity.jdnd.course3.critter.entity.Employee;
 import com.udacity.jdnd.course3.critter.entity.Pet;
 import com.udacity.jdnd.course3.critter.entity.Schedule;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
-
+@Repository
 public interface ScheduleRepository extends JpaRepository<Schedule, Long> {
     List<Schedule>  findAllByPets(Pet pet);
     List<Schedule> findAllByEmployees(Employee employee);
